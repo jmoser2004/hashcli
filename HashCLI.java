c988b3a2d2fc96ebec9e9f82ab6112cdc52d78f9
jmoser2004 <joe@JoeT480.mynetworksettings.com>
2025-03-20 02:39:02 -0400

    Fixed help.

--- HashCLI.java
+++ HashCLI.java
@@ -21,7 +21,17 @@ public class HashCLI
     
     final static String HELP_TEXT = """
 
-                             Help Placeholder
+                             To use this application, write "java HashCLI", followed by your arguments. Your full line should look like this:
+
+                             java HashCLI [input type] [input] [output type] <output>
+
+                             There are two options for input type. They are File and CLI.
+                             For File, the program gets the input from a text file specified in the [input] section.
+                             For CLI, the program gets the input directly from the cli. The input will be the exact text you enter in the [input] section.
+
+                             There are two options for output type. They are File and CLI.
+                             For File, the program writes the output to a text file specified in the [output] section.
+                             For CLI, the program prints the output directly to the terminal. No [output] argument is necessary.
                              """;
 
     public static void main(String[] args)
